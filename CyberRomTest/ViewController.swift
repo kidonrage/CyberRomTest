4cc2c22bb2bdc41458912c1b64fb0df0741ecdec
Vlad Eliseev <v.eliseev@auraband.io>
2022-06-02 15:47:16 +0300

    Initial commit

---
+++ CyberRomTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CyberRomTest
+//
+//  Created by Aura on 02.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
